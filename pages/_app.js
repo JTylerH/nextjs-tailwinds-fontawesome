c84871a07270d5dd5d692e96a9566a97fddd3e54
JTylerH <JTylerH@users.noreply.github.com>
2021-07-24 14:13:19 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBell } from '@fortawesome/free-regular-svg-icons'
-import { faBars, faTimes } from '@fortawesome/pro-regular-svg-icons'
+import { faBell, faBars, faTimes } from '@fortawesome/free-regular-svg-icons'
 library.add(faBell,faBars,faTimes)
 
 function MyApp({ Component, pageProps }) {
